e7784c66c048022f3349ada87496d80845e6e501
Mauricio Manoel <mauriciomanoel@gmail.com>
2018-02-14 19:41:57 -0300

    update

--- get_bibtex.php
+++ get_bibtex.php
@@ -5,8 +5,10 @@
         include $class_name . '.php';
     });
 
-    try {
-        $break_line     = "<br>";
+    $break_line     = "<br>";
+    define('BREAK_LINE', $break_line);
+
+    try {        
         $query_string   = urlencode(trim(@$_GET['query']));
         $file_name      = trim(@$_GET['query']);
         $start          = (int) @$_GET['start'];
@@ -25,15 +27,14 @@
 
         $file           = "bibtex/" . Util::slug(trim($file_name)) . ".bib";
         $url            = GoogleScholar::getURL(0, $query_string);        
-        $cookie         = "1P_JAR=2018-2-14-11; NID=123=Z3b8XLdrlUs5-LhNwjAUEKlFTRqzrQARRmTuJlpzEVPJKBLHgGH4n08Tj8tbXo30Hj3VZc4sZXTlWEiRydVoDE0gZ8Fuw1F1xtXmgHKuL-0tD0wlODBJLg2ItPeF2VIr; GSP=LD=pt-BR:LR=lang_en:NR=20:CF=4:LM=1518608639:S=te4t9p2xzddyHJkW";
+        $cookie         = "1P_JAR=2018-2-14-13; GSP=LD=en:LR=lang_en:NR=20:CF=4:LM=1518616634:S=LBxaKc2xijQOMEC5; NID=123=P4UMpBFf6u-cTBVs6Zpe-SnsraXypD3iU4CL7Gc55QBMKM_U6HcqafQToUU0VyOX_W32GsOOYwUSuTfaDhqrVxJs1LZQCqMjmPJswq5utxoLZ1QfNxOUkqZ4YHwvgQi0";
         // $cookie         = Util::getCookie($url);
 
         $user_agent     = (!empty($_SERVER["HTTP_USER_AGENT"])) ? $_SERVER["HTTP_USER_AGENT"] : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0";
         define('USER_AGENT', $user_agent);
         define('COOKIE', $cookie);
         define('FILE', $file);
-        define('FILE_LOG', "log_" . Util::slug(trim($file_name)) . ".txt");
-        define('BREAK_LINE', $break_line);
+        define('FILE_LOG', "log_" . Util::slug(trim($file_name)) . ".txt");        
         
         if (isset($_GET['page'])) {         
             Util::showMessage("Page: " . $page);
